f2a2998e7cdf2e8bb6dfcfec62fa2ba827786f99
Luc FASQUELLE <mail@riges.net>
2018-03-20 14:00:40 +0100

    remove front version

--- src/store/survey/actions.ts
+++
@@ -1,6 +0,0 @@
-import { DateTime } from 'luxon';
-import { StepCanceled, StepCompleted, SurveyActionKind } from './types';
-
-export const stepCompleted = (step: number): StepCompleted => ({ type: SurveyActionKind.StepCompleted, step, completedAt: DateTime.local() });
-
-export const stepCanceled = (step: number): StepCanceled => ({ type: SurveyActionKind.StepCanceled, step });
